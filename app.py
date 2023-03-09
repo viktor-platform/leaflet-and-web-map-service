e552f83454914330adda88dbc3fa34dbf08a01fa
Jelle van den Berg <jvandenberg@viktor.ai>
2023-03-09 21:48:26 +0200

    Setting up sample map in step 1. Some cleaning up.

--- app.py
+++ app.py
@@ -39,7 +39,8 @@ from viktor.views import WebView, WebResult, DataView, DataResult, DataItem, Dat
 WMS_DEFAULT = "https://service.pdok.nl/wandelnet/regionale-wandelnetwerken/wms/v1_0?version=1.3.0&request=getcapabilities&service=wms"
 
 
-def _get_layer_options(params, **kwargs):
+def _get_layer_options(params: Munch, **kwargs) -> list:
+    """Get layer options from connected WMS-layer"""
     layers = []
     if params.wms_details.wms_input:
         try:
@@ -52,14 +53,16 @@ def _get_layer_options(params, **kwargs):
     return layers
 
 
-def _validate_wms_details(params, **kwargs):
+def _validate_wms_details(params: Munch, **kwargs) -> None:
+    """Validates the WMS input, before the user is allowed to go to the next step."""
     try:
         _ = WebMapService(params.wms_details.wms_input, params.wms_details.wms_version)
     except (RequestException, lxml.etree.XMLSyntaxError):
         raise UserError("Please enter a valid WMS-url first. Click on the button 'Use sample WMS' for an example.")
 
 
-def connect_to_WMS(wms_url, wms_version):
+def connect_to_WMS(wms_url: str, wms_version: str) -> WebMapService:
+    """Connects to the WMS-layers"""
     try:
         wms = WebMapService(wms_url, version=wms_version)
     except RequestException:
@@ -69,7 +72,8 @@ def connect_to_WMS(wms_url, wms_version):
     return wms
 
 
-def get_WMS_details(wms):
+def get_WMS_details(wms: WebMapService) -> dict:
+    """Gets the details from the WMS layer, such as the base url, layers and name."""
     base_url = wms.url
     layers = list(wms.contents)
     name = wms.identification.title
@@ -77,7 +81,9 @@ def get_WMS_details(wms):
 
 
 class Parametrization(ViktorParametrization):
-    introduction = Step("Leaflet", views=["leaflet_introduction"], next_label="WMS set-up")
+    """Parametrization for the sample Leaflet app."""
+
+    introduction = Step("Leaflet", views=["leaflet_introduction"])
     introduction.welcome_text = Text(
         "# Leaflet in VIKTOR  \n"
         "Welcome to the Leaflet app. In this app the advantages of leaflet in VIKTOR are "
@@ -108,13 +114,17 @@ class Parametrization(ViktorParametrization):
         "A WMS (Web Map Service) layer is a type of GIS data layer that allows users to request and "
         "receive map images over the internet.  \n"
         "The main advantage of a WMS-layer is that it ensures the data is always up-to-date, since the "
-        "source of the data is located in one place. Additionally, since WMS is hosted, it is not neceassary to host it yourself.  \n"
+        "source of the data is located in one place. Additionally, since WMS is hosted, it is not neceassary to host "
+        "it yourself.  \n"
         "  \n"
-        "There are a lot of public WMS-layers available. In the Netherlands, [PDOK](https://www.pdok.nl/datasets) is the main provider of public WMS-layers.  \n"
+        "There are a lot of public WMS-layers available. In the Netherlands, [PDOK](https://www.pdok.nl/datasets) is "
+        "the main provider of public WMS-layers.  \n"
         "## WMS in Leaflet  \n"
         "In order to add WMS-layers to Leaflet, we need obtain some information from the WMS-layer first. "
-        "Luckily, we can all do that within this app, making use of the Python package [OWSLib](https://pypi.org/project/OWSLib/)."
-        "We need to obtain the base url, layer names and set the format. For more information, check out the [tutorial](https://leafletjs.com/examples/wms/wms.html).  \n"
+        "Luckily, we can all do that within this app, making use of the Python package "
+        "[OWSLib](https://pypi.org/project/OWSLib/)."
+        "We need to obtain the base url, layer names and set the format. For more information, check out this "
+        "[tutorial](https://leafletjs.com/examples/wms/wms.html).  \n"
         "- **Base url**: The base url is the url of the WMS, without the 'getcapabilities' part. \n"
         "- **Layers**: A WMS-layer can contain multiple layers. For example: streets, houses, etc.  \n"
         "- **Format**: The format in which the layer is retrieved. Most common is png.  \n"
@@ -133,20 +143,25 @@ class Parametrization(ViktorParametrization):
         "Format",
         options=["image/png", "image/jpeg"],
         default="image/png",
-        description="Format of data of the WMS. More options are generally available, but to keep it simple only png and jpg are included in this app. Default is png.",
+        description="Format of data of the WMS. More options are generally available, but to keep it simple only png "
+        "and jpg are included in this app. Default is png.",
     )
     wms_map = Step("Custom WMS", views=["custom_wms_map"])
     wms_map.text = Text(
         "# Custom WMS map  \n"
-        "Now everything is set up, the WMS-layer can be added to the map. Just select the layers to display on the map. Wait for the map "
+        "Now everything is set up, the WMS-layer can be added to the map. Just select the layers to display on the map."
+        " Wait for the map "
         "to reload and the result should appear on the map. That's it!  \n"
         "  \n"
-        "*Note: depending on the WMS-layer you are using, you might need to zoom in or zoom out to see the layer appear on the map.*"
+        "*Note: depending on the WMS-layer you are using, you might need to zoom in or zoom out to see the layer "
+        "appear on the map.*"
     )
     wms_map.layer_options = MultiSelectField("Display layers", options=_get_layer_options)
 
 
 class Controller(ViktorController):
+    """Controller for the sample Leaflet app."""
+
     viktor_enforce_field_constraints = True  # Resolves upgrade instruction https://docs.viktor.ai/sdk/upgrades#U83
 
     label = "My Entity Type"
@@ -154,7 +169,58 @@ class Controller(ViktorController):
 
     @WebView("Leaflet sample map", duration_guess=1)
     def leaflet_introduction(self, params: Munch, **kwargs) -> WebResult:
+        """Create and show a sample leaflet map"""
         m = folium.Map(location=[51.922408, 4.4695292], zoom_start=13)
+        folium.TileLayer(
+            tiles="https://service.pdok.nl/hwh/luchtfotorgb/wmts/v1_0/Actueel_ortho25/EPSG:3857/{z}/{x}/{y}.jpeg",
+            attr='Kaartgegevens &copy; <a href="https://www.kadaster.nl">Kadaster</a>',
+            name="NL luchtfoto 2020",
+            overlay=False,
+            control=True,
+        ).add_to(m)
+        folium.TileLayer(
+            tiles="https://service.pdok.nl/hwh/luchtfotorgb/wmts/v1_0/2018_ortho25/EPSG:3857/{z}/{x}/{y}.jpeg",
+            attr='Kaartgegevens &copy; <a href="https://www.kadaster.nl">Kadaster</a>',
+            name="NL luchtfoto 2018",
+            overlay=False,
+            control=True,
+        ).add_to(m)
+        folium.TileLayer(
+            tiles="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
+            attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/'
+            'licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/'
+            'copyright">OpenStreetMap</a> contributors',
+            name="Watercolor",
+            overlay=False,
+            control=True,
+        ).add_to(m)
+        folium.raster_layers.TileLayer(
+            tiles="https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png",
+            attr='Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors',
+            name="OpenSeaMap",
+            overlay=True,
+            show=True,
+        ).add_to(m)
+        folium.raster_layers.TileLayer(
+            tiles="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
+            attr='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | '
+            'Map style: &copy; <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> '
+            '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+            name="OpenRailWay",
+            overlay=True,
+            show=False,
+        ).add_to(m)
+        folium.raster_layers.WmsTileLayer(
+            url="https://service.pdok.nl/rws/vaarwegmarkeringennld/wms/v1_0",
+            layers=["markdrijvendrd", "markvastrd"],
+            transparent=True,
+            control=True,
+            fmt="image/png",
+            name="Vaarwegmarkeringen WMS",
+            overlay=True,
+            show=True,
+            version="1.3.0",
+        ).add_to(m)
         draw = plugins.Draw(export=True)
         draw.add_to(m)
         folium.LayerControl().add_to(m)
@@ -162,11 +228,13 @@ class Controller(ViktorController):
         m.save(html_result, close_file=False)
         return WebResult(html=StringIO(html_result.getvalue().decode("utf-8")))
 
-    def set_sample_wms(self, params: Munch, **kwargs):
+    def set_sample_wms(self, params: Munch, **kwargs) -> SetParamsResult:
+        """Fills in the sample WMS to the params"""
         return SetParamsResult({"wms_details": {"wms_input": WMS_DEFAULT}})
 
     @DataView("WMS details", duration_guess=1)
-    def show_wms_details(self, params: Munch, **kwargs):
+    def show_wms_details(self, params: Munch, **kwargs) -> DataResult:
+        """Shows the details of the WMS layer, such as the base url, layers and the name."""
         if not params.wms_details.wms_input:
             data = DataGroup(DataItem("Pleas enter a WMS url", None))
         else:
@@ -190,6 +258,7 @@ class Controller(ViktorController):
 
     @WebView("Custom WMS map", duration_guess=1)
     def custom_wms_map(self, params: Munch, **kwargs) -> WebResult:
+        """Creates a map with the WMS-layer, as specified by the user."""
         m = folium.Map(location=[51.922408, 4.4695292], zoom_start=13)
         wms = connect_to_WMS(params.wms_details.wms_input, params.wms_details.wms_version)
         wms_details = get_WMS_details(wms)
